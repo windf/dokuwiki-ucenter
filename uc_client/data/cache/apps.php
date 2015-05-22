db60697bbce21b0d2cb102a04e0a90ba72b8a1e7
linyu <linyu@mxchip.com>
2015-05-22 16:08:33 +0800

    fix bug for uc_api_post

---
+++ uc_client/data/cache/apps.php
@@ -0,0 +1,62 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://dz.com',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => false,
+    'tagtemplates' => '',
+    'allowips' => '',
+  ),
+  2 => 
+  array (
+    'appid' => '2',
+    'type' => 'OTHER',
+    'name' => 'ask',
+    'url' => 'http://example2.com',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '0',
+    'extra' => false,
+    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?>
+<root>
+	<item id="template"><![CDATA[]]></item>
+</root>',
+    'allowips' => '',
+  ),
+  3 => 
+  array (
+    'appid' => '3',
+    'type' => 'OTHER',
+    'name' => 'Dokuwiki',
+    'url' => 'http://example3.com',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '0',
+    'extra' => false,
+    'tagtemplates' => '<?xml version="1.0" encoding="ISO-8859-1"?>
+<root>
+	<item id="template"><![CDATA[]]></item>
+</root>',
+    'allowips' => '',
+  ),
+);
+
+?>
